e10c85c5642b02bab0c06d3785de97119c5f879f
BrandonsLaptop <nbr4nd0n2005@gmail.com>
2026-02-20 10:08:09 +0700

    Fixed The Employment Info Update for Supervisor Role

--- Application/Commands/UpdateEmployeeInfoCommand.cs
+++ Application/Commands/UpdateEmployeeInfoCommand.cs
@@ -8,9 +8,9 @@ namespace Application.Commands;
 public class UpdateEmployeeInfoCommand(int employeeId, UpdateEmploymentInfoDto commandDto) : IRequest<string>
 {
     public int EmployeeId { get; set; } = employeeId;
-    public EmployeeStatus EmploymentStatus { get; set; } = commandDto.EmploymentStatus;
-    public DateTime StartDate { get; set; } = commandDto.StartDate;
-    public EmploymentType EmploymentType { get; set; } = commandDto.EmploymentType;
+    public EmployeeStatus? EmploymentStatus { get; set; } = commandDto.EmploymentStatus;
+    public DateTime? StartDate { get; set; } = commandDto.StartDate;
+    public EmploymentType? EmploymentType { get; set; } = commandDto.EmploymentType;
     public string Department { get; set; } = commandDto.Department;
     public string Position { get; set; } = commandDto.Position;
     public string SupervisorName { get; set; } = commandDto.SupervisorName;
@@ -31,12 +31,12 @@ public class UpdateEmployeeInfoCommand(int employeeId, UpdateEmploymentInfoDto c
                 };
             }
             
-            employee.EmploymentInformation.EmploymentStatus = command.EmploymentStatus;
-            employee.EmploymentInformation.StartDate = command.StartDate;
-            employee.EmploymentInformation.EmploymentType = command.EmploymentType;
-            employee.EmploymentInformation.Department = command.Department;
-            employee.EmploymentInformation.Position = command.Position;
-            employee.EmploymentInformation.SupervisorName = command.SupervisorName;
+            employee.EmploymentInformation.EmploymentStatus = command.EmploymentStatus ?? employee.EmploymentInformation.EmploymentStatus;
+            employee.EmploymentInformation.StartDate = command.StartDate ?? employee.EmploymentInformation.StartDate;
+            employee.EmploymentInformation.EmploymentType = command.EmploymentType ?? employee.EmploymentInformation.EmploymentType;
+            employee.EmploymentInformation.Department = !string.IsNullOrWhiteSpace(command.Department) ? command.Department : employee.EmploymentInformation.Department;
+            employee.EmploymentInformation.Position = !string.IsNullOrWhiteSpace(command.Position) ? command.Position : employee.EmploymentInformation.Position;
+            employee.EmploymentInformation.SupervisorName = !string.IsNullOrWhiteSpace(command.SupervisorName) ? command.SupervisorName : employee.EmploymentInformation.SupervisorName;
             
             await employeeRepository.UpdateEmployeeAsync(employee);
             
